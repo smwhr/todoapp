d2d7b7db44c1c4a24ce4c5965783da241d734845
Ju/TroisYaourts <julien@troisyaourts.com>
2019-10-21 15:16:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Todo App</title>
+</head>
+<body>
+  <h1>Toudoux <small>Pour lister toutes les tâches, surtout les plus douces</small></h1>
+
+</body>
+</html>
