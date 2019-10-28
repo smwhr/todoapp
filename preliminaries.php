ca0f176e3a2e8734b2598abca361d7cdd25d59cc
Ju/TroisYaourts <julien@troisyaourts.com>
2019-10-28 10:54:40 +0100

    Create preliminaries.php

---
+++ preliminaries.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ex 1 : Mettre le titre d'une tache dans une variable $title
+
+// Ex 2 : Mettre l'état d'accomplissement d'une tache dans une variable $done
+
+// Ex 3 : Mettre dans une variable $task un tableau dont les clés sont "title" et "done" les variables précédentes correspondantes
+//=> On appelera ce genre de tableau des "task"s dans la suite
+
+
+
+// Ex
