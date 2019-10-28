d63914e90558fac8a9170954d396169c9669d46e
Ju/TroisYaourts <julien@troisyaourts.com>
2019-10-28 11:46:32 +0100

    Exercices 4 à 8

--- preliminaries.php
+++ preliminaries.php
@@ -2,11 +2,24 @@
 
 // Ex 1 : Mettre le titre d'une tache dans une variable $title
 
+
 // Ex 2 : Mettre l'état d'accomplissement d'une tache dans une variable $done
+
 
 // Ex 3 : Mettre dans une variable $task un tableau dont les clés sont "title" et "done" les variables précédentes correspondantes
 //=> On appelera ce genre de tableau des "task"s dans la suite
 
 
+// Ex 4 : Créer une fonction `get_status` qui prend en argument une "task" et renvoie l'état d'accomplissement
 
-// Ex
+
+// Ex 5 : Créer un tableau vide dans une variable $todo
+
+
+// Ex 6 : Ajouter la tache $task de l'ex 3 à la fin du tableau $todo
+
+
+// Ex 7 : stocker $todo dans la session (clé "todo")
+
+
+// Ex 8 : écrire une boucle foreach pour afficher $todo
